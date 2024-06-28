85ffaeaeb3de8585999d0550e24c4bd8b3c30ac0
Eliúde Vemba <sweeteliude@gmail.com>
2024-06-28 15:21:43 +0100

    Internet connection status above the appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:connectivity_test/internet_controller.dart';
-import 'package:connectivity_test/network_controller.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,16 +13,13 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
-  late NetworkController _networkController;
   late InternetController _internetController;
 
   @override
   void initState() {
     super.initState();
-    _networkController = NetworkController.instance;
     _internetController = InternetController.instance;
 
-    _networkController.init();
     _internetController.init();
   }
 
@@ -39,71 +34,45 @@ class _MainAppState extends State<MainApp> {
         brightness: Brightness.dark,
       ),
       home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            StreamBuilder<List<ConnectivityResult>>(
-              stream: _networkController.connectionStatus,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  final result = snapshot.data!;
-                  final hasWifi = result.contains(ConnectivityResult.wifi);
-                  final hasMobile = result.contains(ConnectivityResult.mobile);
-
-                  return Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Wi-Fi: $hasWifi',
-                          style: const TextStyle(fontSize: 24),
-                        ),
-                        Text(
-                          'Mobile: $hasMobile',
-                          style: const TextStyle(fontSize: 24),
-                        ),
-                      ],
-                    ),
-                  );
-                } else if (snapshot.hasError) {
-                  return Center(
-                    child: Text(
-                      'Error: ${snapshot.error}',
-                      style: const TextStyle(fontSize: 24),
-                    ),
-                  );
-                }
-
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              },
+        body: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: StreamBuilder<bool>(
+                stream: _internetController.connectionStatusStream,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData && snapshot.data!) {
+                    return const SizedBox();
+                  } else {
+                    return const SafeArea(
+                      bottom: false,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('No internet connection'),
+                          SizedBox(width: 10),
+                          CircularProgressIndicator.adaptive(),
+                        ],
+                      ),
+                    );
+                  }
+                },
+              ),
             ),
-            StreamBuilder<bool>(
-              stream: _internetController.connectionStatusStream,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  final result = snapshot.data!;
-
-                  return Center(
-                    child: Text(
-                      'Internet: $result',
-                      style: const TextStyle(fontSize: 24),
-                    ),
+            const SliverAppBar(
+              title: Text('Connectivity Test'),
+              floating: true,
+              pinned: true,
+              snap: true,
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  return ListTile(
+                    title: Text('Item $index'),
                   );
-                } else if (snapshot.hasError) {
-                  return Center(
-                    child: Text(
-                      'Error: ${snapshot.error}',
-                      style: const TextStyle(fontSize: 24),
-                    ),
-                  );
-                }
-
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              },
+                },
+                childCount: 10,
+              ),
             ),
           ],
         ),
